58b5d616bcb77d5c432b01642f5110fad50926b5
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-01-10 10:15:09 +0530

    Made popualr movies responsive for md screens

--- FrontEnd/src/components/1_Home/02_PopularMovies.jsx
+++ FrontEnd/src/components/1_Home/02_PopularMovies.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function PopularMovies({ PopularMoviesList }) {
   return (
-    <div className="grid grid-cols-2 place-items-center place-content-center bg-gray-900 text-white gap-y-5 w-full h-auto">
-      <h1 className="mt-[27px] text-2xl col-span-2 text-center">
+    <div className="grid grid-cols-2 md:grid-cols-3 place-items-center place-content-center bg-gray-900 text-white gap-y-5 w-full h-auto">
+      <h1 className="mt-[27px] text-2xl col-span-2 md:col-span-3 text-center">
         <span>Popular Movies</span>
         <br />
         <span>to Watch Now</span>
       </h1>
-      <span className="text-sm col-span-2">Most watched movies by days</span>
-      <hr className="col-span-2" />
-      <button className="col-span-2 text-gray-600 mb-[27px]">VIEW ALL &rarr;</button>
-      <div className="col-span-2 grid grid-rows-6 grid-cols-2 h-full w-full gap-4 px-4">
+      <span className="text-sm col-span-2 md:col-span-3">Most watched movies by days</span>
+      <hr className="col-span-2 md:col-span-3" />
+      <button className="col-span-2 md:col-span-3 text-gray-600 mb-[27px]">VIEW ALL &rarr;</button>
+      <div className="col-span-2 md:col-span-3 grid grid-rows-6 grid-cols-2 md:grid-rows-4 md:grid-cols-3 h-full w-full gap-4 px-4">
         {PopularMoviesList.map((item, index) => (
           <div key={item.id} className="h-auto w-full">
             <img
@@ -23,7 +23,7 @@ function PopularMovies({ PopularMoviesList }) {
           </div>
         ))}
       </div>
-      <button className="mt-[30px] col-span-2 mb-[70px] border-2 p-3 min-w-[180px]">View More</button>
+      <button className="mt-[30px] col-span-2 md:col-span-3 mb-[70px] border-2 p-3 min-w-[180px]">View More</button>
     </div>
   );
 }
