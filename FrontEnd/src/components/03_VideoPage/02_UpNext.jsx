5473f7cdfd89334d2a5e727066243d295acd240d
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-01-12 21:48:58 +0530

    Optimized more video section

--- FrontEnd/src/components/03_VideoPage/02_UpNext.jsx
+++ FrontEnd/src/components/03_VideoPage/02_UpNext.jsx
@@ -41,13 +41,28 @@ function UpNext() {
             id: 8,
             title: "UFC 300 Main Event",
             thumbnail: "https://vodi.madrasthemes.com/main/wp-content/uploads/sites/2/2019/04/video-thumbnail-8qWC_-4zqWI-339x192.jpg"
+        },
+        {
+            id: 9,
+            title: "UFC 300 Main Event",
+            thumbnail: "https://vodi.madrasthemes.com/main/wp-content/uploads/sites/2/2019/04/video-thumbnail-8qWC_-4zqWI-339x192.jpg"
+        },
+        {
+            id: 10,
+            title: "UFC 300 Main Event",
+            thumbnail: "https://vodi.madrasthemes.com/main/wp-content/uploads/sites/2/2019/04/video-thumbnail-8qWC_-4zqWI-339x192.jpg"
+        },
+        {
+            id: 11,
+            title: "UFC 300 Main Event",
+            thumbnail: "https://vodi.madrasthemes.com/main/wp-content/uploads/sites/2/2019/04/video-thumbnail-8qWC_-4zqWI-339x192.jpg"
         }
     ];
 
     return (
         <div className='w-full col-span-7 lg:col-span-2 p-2 md:px-12 lg:px-2 xl:pr-4'>
             <h1>Up Next</h1>
-            <div className='w-full grid grid-cols-5 p-2 gap-x-2 gap-y-2 overflow-y-scroll max-h-[300px] lg:max-h-[600px]'>
+            <div className='w-full grid grid-cols-5 p-2 gap-x-2 gap-y-2 overflow-y-scroll max-h-[300px] md:max-h-[400px] lg:max-h-[600px]'>
                 {upNextVideos.map((video) => (
                     <React.Fragment key={video.id}>
                         <img 
